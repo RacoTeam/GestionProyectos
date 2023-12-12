9ea1b526fede9e6b26aa3924b3a4e1eefd0674e2
EmanuelHaro <emanuelharo72@gmail.com>
2023-12-12 16:55:43 -0300

    Controlador UsuarioGrupo y UsuarioGrupoTarea con Listar y Obtener

--- Server/Controllers/UsuarioGrupoController.cs
+++ Server/Controllers/UsuarioGrupoController.cs
@@ -8,44 +8,40 @@ using GestionProyectos.Shared.Models;
 using System.Diagnostics;
 
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
 
 namespace GestionProyectos.Server.Controllers
 {
-    /*
+
     [Route("api/[controller]")]
     [ApiController]
     public class UsuarioGrupoController : Controller
     {
         private readonly GestionDeProyectosAdmContext _dbContext;
+        private readonly IMapper _mapper;
 
-
-        public UsuarioGrupoController(GestionDeProyectosAdmContext dbcontext)
+        public UsuarioGrupoController(GestionDeProyectosAdmContext dbcontext,IMapper mapper)
         {
             _dbContext = dbcontext;
+            _mapper = mapper;
         }
 
         [HttpGet]
         [Route("Lista")]
-        public async Task<IActionResult> Lista()
+        public async Task<IActionResult> ListarUsuariosGrupos()
         {
             var responseApi = new ResponseAPI<List<UsuarioGrupoDTO>>();
             var listaUsuarioGruposDTO = new List<UsuarioGrupoDTO>();
             try
             {
-                //foreach (var item in await _dbContext.UsuarioGrupos.Include(d => d.IdUsuarioGrupo).ToListAsync())
-                //{
-                //    listaUsuarioGruposDTO.Add(new UsuarioGrupoDTO
-                //    {
-                //        IdUsuarioGrupo = item.IdUsuarioGrupo,
-                //        UsuarioGrupo1 = item.UsuarioGrupo1,
-                //        Clave = item.Clave,
-                //        Nombre = item.Nombre,
-                //        Apellido = item.Apellido,
-                //        Dni = item.Dni,
-                //        IdRol = item.IdRol,
-                //        //TODO
-                //    });
-                //}
+                var usuariosGruposDb = await _dbContext.UsuarioGrupos.ToListAsync();
+                foreach(var usuarioG in usuariosGruposDb)
+                {
+                    usuarioG.IdUsuarioNavigation = await _dbContext.Usuarios.FirstOrDefaultAsync(u => u.IdUsuario == usuarioG.IdUsuario);
+                    usuarioG.IdUsuarioNavigation.UsuarioGrupos = null;
+
+                    listaUsuarioGruposDTO.Add(_mapper.Map<UsuarioGrupoDTO>(usuarioG));
+                }
 
                 responseApi.EsCorrecto = true;
                 responseApi.Valor = listaUsuarioGruposDTO;
@@ -58,6 +54,39 @@ namespace GestionProyectos.Server.Controllers
 
             return Ok(responseApi);
         }
+
+        [HttpGet]
+        [Route("{idUsuarioGrupo}")]
+        public async Task<ActionResult> ObtenerUsuarioGrupo(int idUsuarioGrupo)
+        {
+            var responseApi = new ResponseAPI<List<UsuarioGrupoDTO>>();
+            var listaUsuarioGruposDTO = new List<UsuarioGrupoDTO>();
+
+            try
+            {
+                var dbUsuarioGrupo = await _dbContext.UsuarioGrupos.Where(f => f.IdUsuario == idUsuarioGrupo).ToListAsync();
+
+                foreach (var usuarioG in dbUsuarioGrupo)
+                {
+                    usuarioG.IdUsuarioNavigation = await _dbContext.Usuarios.FirstOrDefaultAsync(u => u.IdUsuario == usuarioG.IdUsuario);
+                    usuarioG.IdUsuarioNavigation.UsuarioGrupos = null;
+
+                    listaUsuarioGruposDTO.Add(_mapper.Map<UsuarioGrupoDTO>(usuarioG));
+                }
+
+                responseApi.EsCorrecto = true;
+                responseApi.Valor = listaUsuarioGruposDTO;
+
+
+            }
+            catch (Exception ex)
+            {
+                responseApi.EsCorrecto = false;
+                responseApi.Mensaje = ex.Message;
+            }
+
+            return Ok(responseApi);
+        }
     }
-    */
+
 }
---
+++ Server/Controllers/UsuarioGrupoTareaController.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using GestionProyectos.Server.Data;
+using GestionProyectos.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionProyectos.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioGrupoTareaController : Controller
+    {
+        private readonly GestionDeProyectosAdmContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public UsuarioGrupoTareaController(GestionDeProyectosAdmContext dbcontext, IMapper mapper)
+        {
+            _dbContext = dbcontext;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> ListarUsuariosGruposTarea()
+        {
+            var responseApi = new ResponseAPI<List<UsuarioGrupoTareaDTO>>();
+            var listaUsuarioGruposTareaDTO = new List<UsuarioGrupoTareaDTO>();
+            try
+            {
+                var usuariosGruposTareaDb = await _dbContext.UsuarioGrupoTareas.ToListAsync();
+                foreach (var usuarioG in usuariosGruposTareaDb)
+                {
+                    listaUsuarioGruposTareaDTO.Add(_mapper.Map<UsuarioGrupoTareaDTO>(usuarioG));
+                }
+
+                responseApi.EsCorrecto = true;
+                responseApi.Valor = listaUsuarioGruposTareaDTO;
+            }
+            catch (Exception ex)
+            {
+                responseApi.EsCorrecto = false;
+                responseApi.Mensaje = ex.Message;
+            }
+
+            return Ok(responseApi);
+        }
+
+        [HttpGet]
+        [Route("{idUsuarioGrupo}")]
+        public async Task<ActionResult> ObtenerUsuarioGrupoTarea(int idUsuarioGrupo)
+        {
+            var responseApi = new ResponseAPI<List<UsuarioGrupoTareaDTO>>();
+            var listaUsuarioGruposTareaDTO = new List<UsuarioGrupoTareaDTO>();
+
+            try
+            {
+                var dbUsuarioGrupoTarea = await _dbContext.UsuarioGrupoTareas.Where(f => f.IdUsuario == idUsuarioGrupo).ToListAsync();
+
+                foreach (var usuarioG in dbUsuarioGrupoTarea)
+                {
+                    listaUsuarioGruposTareaDTO.Add(_mapper.Map<UsuarioGrupoTareaDTO>(usuarioG));
+                }
+
+                responseApi.EsCorrecto = true;
+                responseApi.Valor = listaUsuarioGruposTareaDTO;
+
+
+            }
+            catch (Exception ex)
+            {
+                responseApi.EsCorrecto = false;
+                responseApi.Mensaje = ex.Message;
+            }
+
+            return Ok(responseApi);
+        }
+    }
+}
